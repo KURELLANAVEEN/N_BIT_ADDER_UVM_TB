c9b0ff6f13fe2b9017294d5d4e9aa548f8afbb26
KURELLANAVEEN <97831047+KURELLANAVEEN@users.noreply.github.com>
2025-08-13 21:12:16 +0530

    comment upddates

--- testbench/adder_sequencer.sv
+++ testbench/adder_sequencer.sv
@@ -1,11 +1,8 @@
-/*
-Coordinates with sequences to send adder_transaction objects to the driver.
-Often written after driver skeleton is done.
-Sequencer is just a thin wrapper, so it’s usually quick.
-*/
+//Coordinates with sequences to send adder_transaction objects to the driver.
 //The uvm_sequencer base class already implements all the necessary sequencer behavior.
 //We usually do not need to add any code or override anything in your custom sequencer class.
 //our sequencer subclass mainly exists to specify the transaction type (adder_transaction) via parameterization and enable factory registration.
+
 class adder_sequencer extends uvm_sequencer #(adder_transaction);
   `uvm_component_utils(adder_sequencer)
   function new(string name = "adder_sequencer", uvm_component parent = null);
