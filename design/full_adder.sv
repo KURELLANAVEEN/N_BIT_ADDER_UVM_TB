356ed3dd715e7a232762f2e119666c37c7ec7cde
KURELLANAVEEN <97831047+KURELLANAVEEN@users.noreply.github.com>
2025-08-13 15:07:21 +0530

    written full adder and half adder rtl code

---
+++ design/full_adder.sv
@@ -0,0 +1,19 @@
+module full_adder(
+  input logic a,
+  input logic b,
+  input logic cin,
+  output logic sum,
+  output logic carry
+);
+
+// We instantiate two half adders for full adder.
+  logic sum1, carry1;
+  half_adder HA_1 (.a(a), .b(b), .sum(sum1), .carry(carry1));
+
+  logic carry2;
+  half_adder HA_1 (.a(sum1), .b(cin), .sum(sum), .carry(carry2));
+
+  assign carry = carry1 | carry2;
+
+endmodule : full_adder
+
